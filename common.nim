011da1465578e32484cf456e2946f73cecabbd68
Jeff Ciesielski <jeff.ciesielski@key.me>
2018-10-25 15:03:43 -0400

    Add symbol object and charToSymbol function

--- common.nim
+++ common.nim
@@ -2,3 +2,20 @@ type
   BFCore* = ref object
    memory*: array[1024, uint8]
    pc*, ap*: int
+
+  BFSymbolKind* = enum
+    bfsApAdjust,
+    bfsMemAdjust,
+    bfsPrint,
+    bfsRead,
+    bfsBlock,
+    bfsBlockEnd,
+    bfsNoOp
+
+  BFSymbol* = object of RootObj
+    case kind*: BFSymbolKind
+    of bfsApAdjust, bfsMemAdjust:
+      amt*: int
+    of bfsBlock:
+      statements*: seq[BFSymbol]
+    else: discard
